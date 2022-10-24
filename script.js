ecc64724f4b8c2658ed5c79b94ecb9e4190f2749
Melissa Johnson <missyj2007@gmail.com>
2022-10-24 18:42:29 -0500

    Added event listener

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+function addingEventListener() {
+    const input = document.getElementById('tile1');
+
+    function changeText() {
+        input.innerHTML = "<p>I've had a passion for coding for many years. It started with simple family websites, then websites for schools and ultimately, I developed a few apps and stand-alone mobile apps. But it was always done as a hobby or a one-off project. Now I want to turn my hobby into a full-time job</p>";
+    }
+    input.addEventListener('click', changeText);
+}
+
+addingEventListener();
